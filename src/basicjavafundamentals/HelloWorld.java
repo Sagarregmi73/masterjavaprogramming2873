e22db3e2c82d23f44a2ca8923daa6b123c39bb72
sagar regmi <sagarregmi026@gmail.com>
2025-02-22 23:28:03 +0545

    added data types

--- src/basicjavafundamentals/HelloWorld.java
+++ src/basicjavafundamentals/HelloWorld.java
@@ -3,4 +3,11 @@ package basicjavafundamentals;
 public class HelloWorld {
 String name="sagar";
 static int num;
+static byte num1=127;
+static double d=127786;
+static char c='k';
+static boolean a=true;
+static float f=45.5f;
+static long l=3457l;
+
 }
--- src/basicjavafundamentals/PrintHelloWorld.java
+++ src/basicjavafundamentals/PrintHelloWorld.java
@@ -5,6 +5,11 @@ public class PrintHelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//implicit type cast or automatic promotion
+		int num1=HelloWorld.num1;
+		
+		//explicit type cast
+		int num2= (int) HelloWorld.d;
 		
 		HelloWorld obj1=new HelloWorld();
 		obj1.name="sagar";
@@ -12,6 +17,14 @@ public class PrintHelloWorld {
 		
 		System.out.println(obj1.name);
 		System.out.println(HelloWorld.num);
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(HelloWorld.c);
+		System.out.println(HelloWorld.d);
+		System.out.println(HelloWorld.f);
+		System.out.println(HelloWorld.l);
+		System.out.println(HelloWorld.a);
+		
 	}
 
 }
